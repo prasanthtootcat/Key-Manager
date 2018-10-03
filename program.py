36f7736f787a23396a43d316882d7d1c3443ea21
prasanthtootcat <prasanthtootcat@gmail.com>
2018-10-03 22:42:26 +0530

    insertion and deletion functions added

---
+++ program.py
@@ -0,0 +1,48 @@
+import pymysql
+import datetime
+
+db = pymysql.connect("localhost","root","","km" )
+cursor = db.cursor()
+
+#pr - private key of user
+#pu - public key of user
+
+# src is used to differentiate key insertion of group and user
+def insert_key(userId, groupId, pu, pr, src):
+    currentDT = datetime.datetime.now()
+    if src == 'g':                      #incase of group key insertion
+        src = groupId
+        val = userId
+    else:
+        src = userId                    #incase of user key insertion
+        val = groupId
+
+    try:
+        tableExist = cursor.execute("SHOW TABLES LIKE '%s'" %(src))
+        if tableExist == 0:
+            cursor.execute("CREATE TABLE IF NOT EXISTS %s (timestamp varchar(50), id varchar(20) primary key, pu varchar(100), pr varchar(100));" %(src))
+        cursor.execute("INSERT INTO %s VALUES('%s', '%s', '%s', '%s')" %(src, currentDT, val, pu, pr))
+        db.commit()
+    except:
+        print('Error in insertion of keys !!!')
+        db.rollback()
+
+
+def delete_key(userId, groupId, src):
+    if src == 'g':                      #incase of group key deletion
+        src = groupId
+        val = userId
+    else:
+        src = userId                    #incase of user key deletion
+        val = groupId
+
+    try:
+        tableExist = cursor.execute("SHOW TABLES LIKE '%s'" %(src))
+        if tableExist == 1:
+            cursor.execute("DELETE FROM %s WHERE id='%s'" %(src, val))
+        db.commit()
+    except:
+        print('Error in deletion of keys !!!')
+        db.rollback()
+
+db.close()
